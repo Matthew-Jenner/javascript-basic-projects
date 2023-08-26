3fcae5d5f571c2f1bf984462cc5a24780b503f93
Matthew Jenner <matthewjenner@hotmail.com>
2023-08-26 08:55:52 +0100

    '5 projects'

--- 01-color-flipper/setup/app.js
+++ 01-color-flipper/setup/app.js
@@ -1,1 +1,15 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colours = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const btn = document.getElementById('btn');
+const colour = document.querySelector('.colour');
+
+btn.addEventListener('click', function(){
+    // get random number between 1 and 3
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+
+    document.body.style.backgroundColor = colours[randomNumber];
+    colour.textContent  = colours[randomNumber];
+})
+function getRandomNumber() {
+    return Math.floor(Math.random() * colours.length);
+}
--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -44,14 +44,10 @@ let currentItem = 0;
 
 // load initial item
 window.addEventListener('DOMContentLoaded', function () {
-  const item = reviews[currentItem];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
+  showPerson(currentItem);
 });
-
 // show person based on item
+
 function showPerson(person) {
   const item = reviews[person];
   img.src = item.img;
@@ -59,26 +55,28 @@ function showPerson(person) {
   job.textContent = item.job;
   info.textContent = item.text;
 }
-// show next person
-nextBtn.addEventListener('click', function () {
-  currentItem++;
-  if (currentItem > reviews.length - 1) {
-    currentItem = 0;
-  }
-  showPerson(currentItem);
-});
-// show prev person
-prevBtn.addEventListener('click', function () {
-  currentItem--;
-  if (currentItem < 0) {
-    currentItem = reviews.length - 1;
-  }
-  showPerson(currentItem);
-});
-// show random person
-randomBtn.addEventListener('click', function () {
-  console.log('hello');
 
-  currentItem = Math.floor(Math.random() * reviews.length);
+// show next person
+nextBtn.addEventListener('click', function() {
+  currentItem++
+  if(currentItem > reviews.length -1)
+  currentItem = 0
+  showPerson(currentItem)
+})
+
+// show prev person 
+prevBtn.addEventListener("click", function () {
+  currentItem--;
+  if(currentItem < 0) {
+    currentItem = reviews.length -1;
+  }
   showPerson(currentItem);
-});
+})
+
+// select random person
+ randomBtn.addEventListener('click', function() {
+ const randomIndex = Math.floor(Math.random() * reviews.length); // Generate a random index
+ showPerson(randomIndex); 
+
+})
+
